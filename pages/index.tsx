06abd6c02479d672093907c52ab4ba12877d4ece
HarunInam <haaruninam@gmail.com>
2022-10-19 17:29:24 +0300

    Changes

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import Navbar from '../components/Navbar'
+
+const Home: NextPage = () => {
+  return (
+    <Navbar />
+  )
+}
+
+export default Home
