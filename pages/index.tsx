84b776b9431262d21e0856d5d3ffd142f84c0db4
HarunInam <haruninam@marun.edu.tr>
2022-10-22 17:13:12 +0300

    files changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import Hello from '/pages/components/Hello'
-import About from '/pages/components/About'
+import Hello from '../components/Hello'
 
 const Home: NextPage = () => {
   return (
